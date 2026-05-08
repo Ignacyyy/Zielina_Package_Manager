0769c7019a308b795beb6c0497cd137516a72fa1
Ignac <ignacyzielinski2008@gmail.com>
2026-05-08 19:45:55 +0200

    Add BLUE color constant to colors.h

--- src/common/colors.h
+++ src/common/colors.h
@@ -11,5 +11,6 @@ inline const std::string YELLOW = "\033[33m";
 inline const std::string CYAN   = "\033[36m";
 inline const std::string BOLD   = "\033[1m";
 inline const std::string RESET  = "\033[0m";
+inline const std::string BLUE   = "\033[1;34m";
 
 #endif
