50ad0fb88db68e319c49a93b45577db7ee3317c7
Ignac <ignacyzielinski2008@gmail.com>
2026-05-08 20:37:51 +0200

    Add files via upload

---
+++ src/common/main.h
@@ -0,0 +1,36 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+// ====================== STANDARD ======================
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <algorithm>
+
+// ====================== SYSTEM ======================
+#include <cstdlib>
+#include <cstdio>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <signal.h>
+#include <atomic>
+#include <thread>
+#include <chrono>
+#include <ctime>
+#include <filesystem>
+#include <map>
+#include <memory>
+
+// ====================== ZPM COMMON ======================
+#include "colors.h"     // colors
+
+
+
+
+
+
+#endif // MAIN_H
