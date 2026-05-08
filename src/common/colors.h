df7fe8f0ee157d9dd4f34451030223c6ee8c5748
Ignac <ignacyzielinski2008@gmail.com>
2026-05-08 19:25:22 +0200

    Add files via upload

---
+++ src/common/colors.h
@@ -0,0 +1,15 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+#include <string>
+
+
+//colors
+inline const std::string RED    = "\033[1;31m";
+inline const std::string GREEN  = "\033[1;32m";
+inline const std::string YELLOW = "\033[33m";
+inline const std::string CYAN   = "\033[36m";
+inline const std::string BOLD   = "\033[1m";
+inline const std::string RESET  = "\033[0m";
+
+#endif
