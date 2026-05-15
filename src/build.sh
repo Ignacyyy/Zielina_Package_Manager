f60e76f947825118f23661d7e25a6543068b9cd4
Ry3ball <109093267+Ry3ball@users.noreply.github.com>
2026-05-15 22:06:04 +0200

    Add files via upload

---
+++ src/build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+SRC_DIR="$(pwd)"
+DEST_DIR="/opt/ZPM"
+
+# create destination directory if it does not exist
+if [ ! -d "$DEST_DIR" ]; then
+    sudo mkdir -p "$DEST_DIR"
+fi
+
+# compile all .cpp files
+for file in "$SRC_DIR"/*.cpp; do
+    [ -e "$file" ] || continue
+
+    filename=$(basename -- "$file")
+    name="${filename%.cpp}"
+
+    echo "Compiling $filename -> $name"
+
+    g++ -O2 "$file" -o "$name"
+
+    if [ $? -eq 0 ]; then
+
+        # move binary to /opt/ZPM
+        sudo mv -f "$name" "$DEST_DIR/"
+
+        # create symlink in /usr/bin
+        sudo ln -sf "$DEST_DIR/$name" "/usr/bin/$name"
+
+        echo "Installed: /usr/bin/$name"
+
+    else
+        echo "Compilation failed: $filename"
+    fi
+done
+
+echo "Done."
